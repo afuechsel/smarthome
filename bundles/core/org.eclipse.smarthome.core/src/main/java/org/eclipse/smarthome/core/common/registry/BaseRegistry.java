6b91f0a24bfcf6daf0fff3f875868cc5acbad747
Michael Bock <BockM@telekom.de>
2017-08-15 16:34:44 +0200

    Patch for Identifiable

--- bundles/core/org.eclipse.smarthome.core/src/main/java/org/eclipse/smarthome/core/common/registry/BaseRegistry.java
+++ bundles/core/org.eclipse.smarthome.core/src/main/java/org/eclipse/smarthome/core/common/registry/BaseRegistry.java
@@ -1,15 +1,3 @@
-/**
- * Copyright (c) 2014,2018 Contributors to the Eclipse Foundation
- *
- * See the NOTICE file(s) distributed with this work for additional
- * information regarding copyright ownership.
- *
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License 2.0 which is available at
- * http://www.eclipse.org/legal/epl-2.0
- *
- * SPDX-License-Identifier: EPL-2.0
- */
 package org.eclipse.smarthome.core.common.registry;
 
 import java.util.Collection;
@@ -19,16 +7,10 @@ import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.Nullable;
 
 /**
- * The {@link Registry} interface represents a registry for elements of the type
- * E. The concrete sub interfaces are registered as OSGi services.
- *
- * @author Dennis Nobel - Initial contribution
- * @author Victor Toni - provide elements as {@link Stream}
- * @author Kai Kreuzer - added null annotations
- *
- * @param <E> type of the elements in the registry
+ * This interface is needed to provide binary compatibility with the former {@Link Registry} interface.
+ * It's completely the same interface as {@Link Registry} with the type restriction E extends Identifiable.
  */
-public interface Registry<E extends Identifiable<K>, K> extends BaseRegistry<E, K> {
+public interface BaseRegistry<E, K> {
 
     /**
      * Adds a {@link RegistryChangeListener} to the registry.
@@ -55,7 +37,8 @@ public interface Registry<E extends Identifiable<K>, K> extends BaseRegistry<E, 
     /**
      * This method retrieves a single element from the registry.
      *
-     * @param key key of the element
+     * @param key
+     *            key of the element
      * @return element or null if no element was found
      */
     public @Nullable E get(K key);
@@ -63,36 +46,43 @@ public interface Registry<E extends Identifiable<K>, K> extends BaseRegistry<E, 
     /**
      * Removes a {@link RegistryChangeListener} from the registry.
      *
-     * @param listener registry change listener
+     * @param listener
+     *            registry change listener
      */
     void removeRegistryChangeListener(@NonNull RegistryChangeListener<E> listener);
 
     /**
      * Adds the given element to the according {@link ManagedProvider}.
      *
-     * @param element element to be added (must not be null)
+     * @param element
+     *            element to be added (must not be null)
      * @return the added element or newly created object of the same type
-     * @throws IllegalStateException if no ManagedProvider is available
+     * @throws IllegalStateException
+     *             if no ManagedProvider is available
      */
     public @NonNull E add(@NonNull E element);
 
     /**
      * Updates the given element at the according {@link ManagedProvider}.
      *
-     * @param element element to be updated (must not be null)
+     * @param element
+     *            element to be updated (must not be null)
      * @return returns the old element or null if no element with the same key
      *         exists
-     * @throws IllegalStateException if no ManagedProvider is available
+     * @throws IllegalStateException
+     *             if no ManagedProvider is available
      */
     public @Nullable E update(@NonNull E element);
 
     /**
      * Removes the given element from the according {@link ManagedProvider}.
      *
-     * @param key key of the element (must not be null)
+     * @param key
+     *            key of the element (must not be null)
      * @return element that was removed, or null if no element with the given
      *         key exists
-     * @throws IllegalStateException if no ManagedProvider is available
+     * @throws IllegalStateException
+     *             if no ManagedProvider is available
      */
     public @Nullable E remove(@NonNull K key);
 }
